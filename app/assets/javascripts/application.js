234542b65377ebd3d7c082113ffe364ef6667836
hiimtaylorjones <taylorjonessoftware@gmail.com>
2016-08-28 11:41:04 -0400

    ember bootstrapping stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,16 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require ./environment
+//= require ember
+//= require ember-data
+//= require active-model-adapter
+
+//= require_self
+//= require ./black-hole
+
+// for more details see: http://emberjs.com/guides/application/
+BlackHole = Ember.Application.create();
 //= require_tree .
