862fab01ebf105449dedfed3abd9e8c1d481e611
hiimtaylorjones <taylorjonessoftware@gmail.com>
2016-11-12 22:54:51 -0500

    setting up devise for ember

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount_ember_app :frontend, to: "/"
 
+  devise_for :users, controllers: { sessions: 'sessions' }
+
   namespace :api do
     namespace :v1 do
       resources :posts
